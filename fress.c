046a76261dee342bff36039a1f96ae826e828764
FranQ <frankchristopherquispe@gmail.com>
2022-05-12 14:19:22 +0000

    fress: contains _free function that frees a double pointer.

---
+++ fress.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * _free - frees double pointer
+ * @av: input double pointer
+ *
+ * Return: void
+ */
+
+void _free(char **av)
+{
+	int i = 0;
+
+	while (av[i])
+	{
+		free(av[i]);
+		i++;
+	}
+	free(av);
+}
+
