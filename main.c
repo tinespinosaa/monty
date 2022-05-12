90d99cf90aa93e826ad602ae6bfccc2031ca2c15
FranQ <frankchristopherquispe@gmail.com>
2022-05-12 15:47:08 +0000

    main.c gets resize

--- main.c
+++ main.c
@@ -2,45 +2,22 @@
 
 int main(int argc, char *argv[])
 {
-	FILE *stream;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	unsigned int index = 0;
-	char *command = NULL, *data = NULL, **tokens = NULL;
+	FILE *pfile;
+	int exit_status = EXIT_SUCCESS;
 
 	/* no more than arguments */
 	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		return (EXIT_FAILURE);
-	}
+		return (usage_error());
 
 	/* testeo del archivo */
-	stream = fopen(argv[1], "r");
-	if (stream == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s", argv[1]);
-		return (EXIT_FAILURE);
-	}
+	pfile = fopen(argv[1], "r");
+	if (pfile == NULL)
+		return (open_failure(argv[1]));
 
-	/* lectura del archivo */
-	while ((nread = getline(&line, &len, stream)) != -1)
-	{
-		tokens = tokenizer(line, " ");
-		command = tokens[0];
-		data = tokens[1];
-
-		printf("linea %u: %s", index, line);
-		printf("comando: %s\n", command);
-		printf("data: %s\n", data);
-		_free(tokens);
-		index++;
-	}
-
+	/* lectura del archivo y ejecucion de monty */
+	exit_status = monty_exec(pfile);
 
 	/* final */
-	free(line);
-	fclose(stream);
-	return (EXIT_SUCCESS);
+	fclose(pfile);
+	return (exit_status);
 }
